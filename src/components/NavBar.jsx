cdf7ec3e3805c678ea4af1370c76d04ea619cf8d
dpmehta <dpmehta2306@gmail.com>
2024-05-03 10:34:52 +0530

    made proper alignment of NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -15,37 +15,32 @@ export default function NavBar() {
   };
   return (
     <>
-      <header className="header top-0 bg-white shadow-md flex flex-col sm:flex-row items-center justify-between px-4 sm:px-6 lg:px-8 py-1 sticky top-0 z-50">
-        <h1 className="w-full sm:w-4/12 sm:mb-0">
-          <a href="/">
-            <img src={MuLogo} alt="logo" className="w-50 mx-5 sm:mx-0" />
-          </a>
-        </h1>
-        <nav className="nav font-semibold text-lg flex-grow">
+      <header className="header top-0 bg-white shadow-md flex flex-col sm:flex-row items-center justify-between px-4 sm:px-6 lg:px-8 py-2 sticky top-0 z-50">
+        <img src={MuLogo} alt="logo" className="w-40 sm:mr-0" />
+
+        <nav className="nav ml-60 font-semibold text-lg flex-grow">
           <ul className="flex items-center justify-center sm:justify-end">
-            <li className="p-4 b-2">
+            <li className="p-3 b-2 flex-shrink-0">
               <Link to="/home">Home</Link>
             </li>
-            <li className="p-4 b-2">
-              <Link to="/lab-entry">Lab Entry</Link>
-            </li>
-            <li className="p-4 b-2">
+            <li className="p-3 b-2 flex-shrink-0">
               <Link to="/component-issue">Component Issue</Link>
             </li>
-            <li className="p-4 b-2">
+            <li className="p-3 b-2 flex-shrink-0">
+              <Link to="/lab-availability">Lab Availability</Link>
+            </li>
+            <li className="p-3 b-2 flex-shrink-0">
               <Link to="/lab-overview">Lab Explore</Link>
             </li>
-            <li className="p-4 b-2">
+            <li className="p-3 b-2 flex-shrink-0">
               <Link to="/request-show">My Requests</Link>
             </li>
-            <li className="p-4 b-2">
-              <Link to="/component-request-review">Request Review</Link>
-            </li>
-            <li className="p-4 b-2">
+
+            <li className="p-3 b-2 flex-shrink-0">
               <Link to="/complaint">Add Complaint</Link>
             </li>
-            <li className="p-4 b-2">
-              <Link to="/lab-availability">Lab Availability</Link>
+            <li className="p-3 b-2 flex-shrink-0">
+              <Link to="/complaint-review">My Complaints</Link>
             </li>
           </ul>
         </nav>
@@ -55,6 +50,11 @@ export default function NavBar() {
               onClick={handleLogout}
               type="button"
               className="btn btn-info mb-3 mr-3"
+              style={{
+                backgroundColor: "#14a2b9",
+                borderColor: "#14a2b9",
+                color: "white",
+              }}
             >
               Logout
             </button>
