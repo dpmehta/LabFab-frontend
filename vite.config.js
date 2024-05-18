d235c5147e7e5fca855c89eeb80bf49c62b435cf
dpmehta <dpmehta2306@gmail.com>
2024-05-18 23:08:25 +0530

    changed some configuration for hosting purpose

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    fs: {
-      allow: [
-        "E:/Project-AWT/LabFab/backend/assets/images/component-complaint-images",
-        "E:/Project-AWT/LabFab/backend/assets/images/computer-complaint-images",
-        "E:/Project-AWT/LabFab/frontend/src",
-      ],
-    },
-  },
 });
